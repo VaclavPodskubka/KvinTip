ea8a91860c58a377ef3b93e0e4ca8679dd43c422
vaclavpodskubka <mrtiktokme@gmail.com>
2026-06-12 20:22:06 +0200

    Uložení všech změn

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,19 +1,21 @@
-import { initializeApp } from 'firebase/app'
-import { getAuth, GoogleAuthProvider, setPersistence, browserLocalPersistence } from 'firebase/auth'
-import { getFirestore } from 'firebase/firestore'
+// lib/firebase.ts
+import { initializeApp } from 'firebase/app';
+import { getAuth, GoogleAuthProvider, setPersistence, browserLocalPersistence } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
 
-const firebaseConfig = {
+export const firebaseConfig = {
   apiKey: "AIzaSyAM2V_YgVEpZgz-uDXwQ4jxEfwhOSsJQUA",
   authDomain: "kvintip-dfa19.firebaseapp.com",
   projectId: "kvintip-dfa19",
   storageBucket: "kvintip-dfa19.firebasestorage.app",
   messagingSenderId: "533176587446",
   appId: "1:533176587446:web:b030dc7abb3c44a2af4217"
-}
+};
 
-const app = initializeApp(firebaseConfig)
-export const auth = getAuth(app)
-export const provider = new GoogleAuthProvider()
-export const db = getFirestore(app)
+// Exportujeme 'app', což je ta správná inicializovaná FirebaseApp
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const provider = new GoogleAuthProvider();
+export const db = getFirestore(app);
 
-setPersistence(auth, browserLocalPersistence)
+setPersistence(auth, browserLocalPersistence);
